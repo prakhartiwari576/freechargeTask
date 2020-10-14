b50ff282ce67613dbbe25f9edd719739f322ea9d
vasujain314 <jain.vasu509@gmail.com>
2020-10-14 19:05:09 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ var express = require('express');
 
 const csvRouter = require('./router/upload');
 
-
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(port, () => {
 
+app.use(csvRouter);
+app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
--- src/router/upload.js
+++ src/router/upload.js
@@ -3,14 +3,21 @@ const jwt = require('jsonwebtoken');
 var express = require('express');
 const router = new express.Router();
 const auth = require('../middleware/Auth');
-
-router.get('/upload',(req,res)=>{
-    var token = req.header('Authorization');
-    const decoded = jwt.verify(token, 'freecharge');
+var upload = require("express-fileupload");
+var csvtojson = require("csvtojson");
 
 
+router.post('/upload',(req,res)=>{
+    // var token = req.header('Authorization');
+    // const decoded = jwt.verify(token, 'freecharge');
 
-     
+    // console.log(req.body);
+    // res.send(req.body);
+
+    var csvData = req.file;
+    console.log(csvData);
+    // return csvtojson().fromString(csvData).then(json => 
+    //   {return res.status(201).json({csv:csvData, json:json})})  
 })
 
 module.exports = router;
