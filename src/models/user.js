6299349505f6febec1e8828a98f83ddebdd024c3
vasujain314 <jain.vasu509@gmail.com>
2020-10-14 17:36:23 +0530

    models and auth

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,11 @@
-//User Model
+
+const mongoose = require('mongoose')
+const user = new mongoose.Schema({
+    Name:Number,
+    username:String,
+    password:Number,
+    accountnumber:String
+})
+const user = mongoose.model('User',user)
+user.createIndexes();
+module.exports = user;
