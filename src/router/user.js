1b8b4ea3f10ab48013c938e889cd6ac6654c163d
PRAKHAR TWAIRI <prakhartiwari576@gmail.com>
2020-10-14 19:00:47 +0530

    'savingInDB'

--- src/router/user.js
+++ src/router/user.js
@@ -8,14 +8,13 @@ router.post('/users',async (req,res)=>{
   var accountnumber = str.slice(0,4) + Math.floor(1000 + Math.random() * 9000);
 
   const user = new User({...req.body,accountnumber:accountnumber})
-  console.log(user)
-  // try{
-  //   await user.save()
-  //   // const token = await user.generateAuthToken()
-  //   res.status(201).send({user})
-  // }catch(error){
-  //   res.status(400).send(error)
-  // }
+  try{
+    await user.save()
+    // const token = await user.generateAuthToken()
+    res.status(201).send({user})
+  }catch(error){
+    res.status(400).send(error)
+  }
 })
 
 module.exports = router
